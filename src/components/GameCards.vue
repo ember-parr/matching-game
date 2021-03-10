3ea8b82261dec1bc986a9e55d46d8eff14a90ee6
Ember Parr <ember-devops@gmail.com>
2021-03-10 14:36:39 -0600

    card names display on dom and shuffle with each refresh

---
+++ src/components/GameCards.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h4>{{ card.name }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["card", "image"],
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped></style>
--- src/components/MatchingGame.vue
+++ src/components/MatchingGame.vue
@@ -2,23 +2,37 @@
   <div>
     <h3>game on!</h3>
     <div class="matchingGame">
-      <div v-for="card in CardData" :key="card.id" class="singleCard">
-        <game-cards :card="card" @flip-card="flipCard" />
+      <div v-for="card in languages" :key="card.id" class="singleCard">
+        <game-cards
+          :card="card"
+          :image="require(`../assets/cards/${card.image}`)"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import shuffle from "lodash.shuffle";
 import { CardData } from "../assets/CardData.js";
 import GameCards from "./GameCards.vue";
+import shuffle from "lodash.shuffle";
 export default {
   components: { GameCards },
   data() {
-    shuffle(CardData);
+    return {
+      languages: shuffle([...CardData]),
+      guessOne: null,
+      guessTwo: null
+    };
   }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.matchingGame {
+  border: 2px solid green;
+}
+.singleCard {
+  border: 2px solid blue;
+}
+</style>
