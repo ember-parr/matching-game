bea86aa6bf39679d058ecbd86f3335d516665d9b
Ember Parr <ember-devops@gmail.com>
2021-03-10 14:09:08 -0600

    began game function

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Game Hero Image" src="./assets/matching-hero_image.jpg" class="hero-image" />
+    <img
+      alt="Game Hero Image"
+      src="./assets/matching-hero_image.jpg"
+      class="hero-image"
+    />
+    <div>
+      <matching-game />
+    </div>
   </div>
 </template>
 
 <script>
-
+import MatchingGame from "./components/MatchingGame.vue";
 export default {
   name: "App",
   components: {
+    MatchingGame
   }
 };
 </script>
---
+++ src/components/MatchingGame.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>game on!</h3>
+    <div class="matchingGame">
+      <div v-for="card in CardData" :key="card.id" class="singleCard">
+        <game-cards :card="card" @flip-card="flipCard" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import shuffle from "lodash.shuffle";
+import { CardData } from "../assets/CardData.js";
+import GameCards from "./GameCards.vue";
+export default {
+  components: { GameCards },
+  data() {
+    shuffle(CardData);
+  }
+};
+</script>
+
+<style scoped></style>
